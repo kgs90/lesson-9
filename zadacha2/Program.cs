3edd55586480e1893a7cbb2e5de8620438dc5663
kgs90 <kgs90@inbox.ru>
2022-06-14 19:07:33 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿int sum = 0;
+
+Console.WriteLine("Запишите M и N через пробел: ");
+int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Console.WriteLine("Сумма натуральных чисел между указанными числами: ");
+Console.WriteLine(summary(size[0] + 1, size[1]));
+
+int summary(int min, int max) 
+{
+    if (Math.Abs(max - min) > 0) 
+    {
+        sum = sum + min;
+        min = min + 1;
+        summary(min, max);
+    }
+    return sum;
+}
