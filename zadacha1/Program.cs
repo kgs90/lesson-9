28ef50486b47dfe0f1f0fb151c4908924edee3e4
kgs90 <kgs90@inbox.ru>
2022-06-14 19:04:32 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = 0, sum = 0;
+
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр в числе: ");
+Console.WriteLine(count_sum(number));
+
+int count_sum(int num) 
+{
+    if (num  > 9) 
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+        count_sum(num);
+    } else 
+    {
+        sum = sum + num;
+    }
+    return sum;
+}
