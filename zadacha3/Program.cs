581ad9d37019931be5948d437bed031c1fe39ca5
kgs90 <kgs90@inbox.ru>
2022-06-14 19:12:11 +0300

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Запишите неотрицательные числа m и n через пробел: ");
+int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Console.WriteLine("Значение функции Аккермана: ");
+Console.WriteLine(Ackerman_func(Math.Abs(size[0]), Math.Abs(size[1])));
+
+Int64 Ackerman_func(Int64 n, Int64 m) 
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Ackerman_func(n - 1, 1);
+    else
+      return Ackerman_func(n - 1, Ackerman_func(n, m - 1));
+}
